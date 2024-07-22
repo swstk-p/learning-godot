5249982c993c96eb146a162415ee3ac0efa0fa52
Swastik Pandey <swastik.pandey@deerwalk.edu.np>
2024-07-22 10:23:53 +0545

    chore: fix output console issue

--- scripts/coin.gd
+++ scripts/coin.gd
@@ -3,7 +3,7 @@ extends Area2D
 var og_scale = scale
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	print("here") # Replace with function body.
+	pass# Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -14,4 +14,5 @@ func _process(_delta):
 
 
 func _on_body_entered(_body):
+	print("Plus one coin")
 	scale = og_scale if scale==og_scale*2 else og_scale*2
